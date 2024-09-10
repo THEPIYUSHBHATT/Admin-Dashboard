434a995c90f44c35513b43f96bbb45981c8ddb20
piyush <piyushbhatt053@gmail.com>
2024-09-10 23:57:54 +0530

    changes

--- admin-dashboard/dashboard/src/App.tsx
+++ admin-dashboard/dashboard/src/App.tsx
@@ -1,11 +1,25 @@
-import { BrowserRouter as Router , Routes, Route } from "react-router-dom"
-import Dashboard from "./pages/Dashboard"
+import { BrowserRouter as Router , Routes, Route } from "react-router-dom";
+import {lazy, Suspense} from 'react';
+
+import Loader from "./components/Loader"
+const Dashboard = lazy(() => import('./pages/Dashboard'))
+const Products = lazy(() => import('./pages/Products'))
+const Transaction = lazy(() => import('./pages/Transaction'))
+const Customer = lazy(() => import('./pages/Customer'))
 
 const App = () => {
   return (
     <Router>
       <Routes>
-         <Route path="/admin/dashboard" element={<Dashboard />} />
+        <Suspense fallback={ <Loader />}>
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+
+          <Route path="/admin/product" element={<Products />} />
+
+          <Route path="/admin/transaction" element={<Transaction />} />
+
+          <Route path="/admin/customer" element={<Customer />} />
+        </Suspense>
       </Routes>
     </Router>
   )
